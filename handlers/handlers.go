26e109f68e1a8c0f9d27c7f8df22c11f0da63ed0
Martijn Korbee <mkorbee@outlook.com>
2022-11-22 11:01:14 +0100

    removed course session

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -20,10 +20,6 @@ func (h *Handlers) Home(w http.ResponseWriter, r *http.Request) {
 
 // Sessions is the sessions route
 func (h *Handlers) SessionsTest(w http.ResponseWriter, r *http.Request) {
-	myData := "foo"
-
-	h.App.Session.Put(r.Context(), "bar", myData)
-
 	err := h.App.Render.Page(w, r, "sessions", nil, nil)
 	if err != nil {
 		h.App.ErrorLog.Println("error rendering", err)
